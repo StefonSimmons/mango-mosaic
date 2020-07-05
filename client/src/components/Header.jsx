9e39b1906a47a0ce077380af431b42920d7bcb3d
stefonsimmons <s.j.simmons@spartans.nsu.edu>
2020-07-05 19:22:00 -0400

    added reset.css

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Logo = styled.img`
-  width: 185px;
-  height: 171px
+  width: 150px;
+  height: 139px
+`
+const LogoName = styled.h1`
+  font-family: 'Dancing Script', cursive;
 `
 
 
@@ -11,6 +14,7 @@ export default function Header() {
   return (
     <div>
       <Logo src="https://imgur.com/DMBxMaA.png" alt="mango-mosaic-logo"/>
+      <LogoName>Mango Mosaic</LogoName>
     </div>
   )
 }
