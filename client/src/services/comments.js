aee5b33d6c21c8d4ecd76395f4da654edd11c5a7
stefonsimmons <s.j.simmons@spartans.nsu.edu>
2020-07-05 18:37:54 -0400

    added comment functions

---
+++ client/src/services/comments.js
@@ -0,0 +1,16 @@
+import api from './apiConfig';
+
+export const getAllComments = async () => {
+  const resp = await api.get('/comments')
+  const comments = resp.data
+
+  return comments
+}
+
+
+export const createComment = async (commentParams) => {
+  const resp = await api.post('/comments', {comment: commentParams})
+  const newComment = resp.data
+
+  return newComment
+}
