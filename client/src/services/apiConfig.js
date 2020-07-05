19f2947c78d71e9aa3a41a14085444ed84985e0a
stefonsimmons <s.j.simmons@spartans.nsu.edu>
2020-07-05 18:25:50 -0400

    added auth functions and apiConfig

---
+++ client/src/services/apiConfig.js
@@ -0,0 +1,10 @@
+import axios from 'axios'
+
+// api/server runs on localhost 3000 in development
+const baseUrl = process.env.NODE_ENV === 'production' ? 'https://mango-mosaic-api.herokuapp.com/' : 'http://localhost:3000' 
+
+const api = axios.create({
+  baseURL: baseUrl
+})
+
+export default api;
---
+++ client/src/services/auth.js
@@ -0,0 +1,28 @@
+  
+import api from './apiConfig';
+
+export const loginUser = async (loginParams) => {
+
+  const resp = await api.post('/auth/login', { auth: loginParams })
+  const token = resp.data.token
+  const admin = resp.data.user
+  localStorage.setItem('authToken', token);
+  api.defaults.headers.common.authorization = `Bearer ${token}`
+  return admin
+}
+
+export const verifyUser = async () => {
+  const token = localStorage.getItem('authToken');
+  if (token) {
+    api.defaults.headers.common.authorization = `Bearer ${token}`
+    const resp = await api.get('/auth/verify');
+    const admin = resp.data
+    return admin
+  }
+  return false
+}
+
+// logging out
+export const removeToken = () => {
+  api.defaults.headers.common.authorization = null;
+}
