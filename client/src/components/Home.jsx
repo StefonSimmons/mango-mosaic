e08eb0af70e2bf0526e38b663797eee9e1cc73c4
stefonsimmons <s.j.simmons@spartans.nsu.edu>
2020-07-08 10:24:03 -0400

    framework of homepage

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -3,31 +3,51 @@ import styled from 'styled-components'
 
 const Main = styled.div`
   display: flex;
+  align-items: center;
+  justify-content: flex-start;
   padding: 15px 75px ;
   height: 75vh
 `
-const HomeImg = styled.img`
-  width: 400px;
-  height: 400px;
+const Wrapper = styled.div`
+  display: flex;
   position: absolute;
-  align-self: center;
+`
+const HomeImg = styled.img`
+  width: 500px;
+  height: 500px;
+`
+const ContentWrapper = styled.div`
+  width: 450px;
+  margin-left: 50px;
+`
+const ContentTitle = styled.h1`
+  font-family: 'Dancing Script', cursive;
+  font-size: 48px;
+  letter-spacing: 2px;
+  margin-bottom: 15px;
+`
+const Content = styled.p`
+  font-family: 'Open Sans Condensed', sans-serif;
+  font-size: 18px;
+  letter-spacing: 2px;
+  line-height: 1.75;
 `
 const RedSquare = styled.div`
   background-color: #972309;
-  width: 400px;
-  height: 400px;
+  width: 500px;
+  height: 500px;
   align-self: flex-end;
-  margin-left: 15%;
+  margin-left: 25%;
   z-index: -1;
   opacity: .2;
 `
 const GreenSquare = styled.div`
   background-color: #2B791E;
-  width: 400px;
-  height: 400px;
-  position: absolute;
-  top: 25%;
-  left: 45%;
+  width: 500px;
+  height: 500px;
+  position: relative;
+  top: -8%;
+  left: -8%;
   opacity: .2;
 `
 
@@ -36,7 +56,13 @@ const GreenSquare = styled.div`
 export default function Home() {
   return (
     <Main>
-      <HomeImg src='https://imgur.com/KrfGjBX.png' alt='mango-home' />
+      <Wrapper>
+        <HomeImg src='https://imgur.com/KrfGjBX.png' alt='mango-home' />
+        <ContentWrapper>
+          <ContentTitle>This blog...</ContentTitle>
+          <Content>Lorem IContentsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</Content>
+        </ContentWrapper>
+      </Wrapper>
       <RedSquare>
       </RedSquare>
       <GreenSquare>
