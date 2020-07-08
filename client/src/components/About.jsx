83e73f41cd61b7cb9cfbbbce3c264e402cca5cf9
stefonsimmons <s.j.simmons@spartans.nsu.edu>
2020-07-08 15:41:39 -0400

    added image to about me

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -1,9 +1,40 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-around;
+  margin-top: 30px
+`
+const Photo = styled.img`
+  width: 500px;
+  position: absolute;
+  top: 25%;
+  left: 15%;
+`
+const RedSquare = styled.div`
+  background-color: #972309;
+  width: 500px;
+  height: 500px;
+  opacity: .2;
+  z-index: -1
+`
+const YellowSquare = styled.div`
+  background-color: #CBB334;
+  width: 500px;
+  height: 500px;
+  opacity: .2;
+  z-index: -1
+`
 
 export default function About() {
   return (
-    <div>
-      ok
-    </div>
+    <Wrapper>
+      <Photo src= 'https://imgur.com/s9JH7E3.png' alt='ashlea'/>
+      <RedSquare>
+      </RedSquare>
+      <YellowSquare>
+      </YellowSquare>
+    </Wrapper>
   )
 }
