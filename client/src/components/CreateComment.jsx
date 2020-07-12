bd9ce98931b0efd0c4888a3a08813af371a67a3a
stefonsimmons <s.j.simmons@spartans.nsu.edu>
2020-07-12 16:45:05 -0400

    styled comment create box

--- client/src/components/CreateComment.jsx
+++ client/src/components/CreateComment.jsx
@@ -1,21 +1,65 @@
 import React from 'react'
-import {Divider} from './Comment'
+import { Divider } from './Comment'
+import styled from 'styled-components'
 
+const CommentForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  width: 700px;
+  padding: 20px;
+  background-color: #E7E7EF;
+  font-family: 'Open Sans Condensed', sans-serif;
+  font-size: 16px;
+  font-weight: 400;
+  letter-spacing: 2px
+`
+const InputWrapper = styled.div`
+  display: flex;
+  align-items: flex-end;
+  justify-content: flex-end
+  width: 500px
+`
+const Input = styled.input`
+  margin: 5px;
+  width: 220px;
+  height: 33px
+`
+const NameEmailFields = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  margin-right: 10px
+
+`
+const CommentArea = styled.textarea`
+  margin-top: 5px;
+  height: 77px
+`
 export default function CreateComment() {
 
   return (
     <div>
       <Divider />
 
-      <form>
-        <label htmlFor="name">Name</label>
-        <input id="name" type="text" />
-        <label htmlFor="email">Email</label>
-        <input id="email" type="text" />
-        <label htmlFor="social">Social</label>
-        <input id="social" type="text" />
-        <textarea type="text" placeholder='Write Your Comment Here...'/>
-      </form>
+      <CommentForm>
+        <InputWrapper>
+          <NameEmailFields>
+            <div>
+              <label htmlFor="name">Name:</label>
+              <Input id="name" type="text" placeholder="Your Name" />
+            </div>
+            <div>
+              <label htmlFor="email">Email:</label>
+              <Input id="email" type="text" placeholder="(Not published)" />
+            </div>
+          </NameEmailFields>
+          <div>
+            <label htmlFor="social">Social:</label>
+            <Input id="social" type="text" placeholder="@Instagram or @Twitter" />
+          </div>
+        </InputWrapper>
+        <CommentArea type="text" placeholder='Write Your Comment Here...' />
+      </CommentForm>
     </div>
   )
 }
