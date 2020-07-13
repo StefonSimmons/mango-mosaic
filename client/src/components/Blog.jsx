cf1eb28e136902d1caef224f87e892374d0279e6
stefonsimmons <s.j.simmons@spartans.nsu.edu>
2020-07-13 18:28:17 -0400

    created a better grid layout for the blog pg

--- client/src/components/Blog.jsx
+++ client/src/components/Blog.jsx
@@ -3,49 +3,52 @@ import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 
 
-const Main = styled.div`
-  margin: 0 auto;
-  width: 70%;
-  // background: rgba(208, 235, 45, .7);
-`
+// const Main = styled.div`
+//   display: grid;
+//   justify-items: center;
+// `
 const PostsWrapper = styled.div`
   display: grid;
-  grid-template: auto/auto auto auto;  
-  justify-content: center;
-`
-const PostLink = styled(Link)`
-  text-decoration: none;
-  color: black;
+  grid-template-columns: repeat(3, 1fr);
+  margin: 0 auto;
+  width: 70%;  
+  justify-items: center;
 `
 const Post = styled.div`
-  margin: 20px 25px;
-  align-items: start;
-  height: 300px
+  display: grid;
+  grid-template-columns: repeat(10,1fr);
+  grid-template-rows: repeat(17,1fr);
+  height: 300px;
+  width: 300px; 
+`
+const Item1 = styled.div`
+  grid-column: 1 / span 10;
+  grid-row: 1 / span 8;
 `
 const BlogID = styled.p`
   font-family: 'Dancing Script', cursive;
   font-size: 32px;
-  margin-bottom: 20px
-`
-const Wrapper = styled.div`
-  // background: rgba(208, 235, 45, .7);
-  width: 295px;
+  margin-bottom: 20px;
 `
 const PostImg = styled.img`
   width: 250px;
   height: 137px
 `
-const ContentWrapper = styled.div`
-  // background: blue;
-  padding-left: 20px;
-  height: 70px;
+const PostLink = styled(Link)`
+  text-decoration: none;
+  color: black;
+  display: hidden;
+`
+const TitleWrapper = styled.div`
+  grid-column: 3 / 11;
+  grid-row: 13 / span 2;
+  z-index: 2
 `
 const PostTitle = styled.h1`
   font-family: 'Open Sans Condensed', sans-serif;
   font-weight: 700;
   font-size: 18px;
   letter-spacing: 2px;
-  margin-bottom: 10px
 `
 const PostSubTitle = styled.h2`
   font-family: 'Open Sans Condensed', sans-serif;
@@ -55,38 +58,39 @@ const PostSubTitle = styled.h2`
 `
 const ColorSquare = styled.div`
   background-color: #EF7218;
-  width: 250px;
-  height: 259px;
-  top: -75%;
-  left: 10%;
-  position: relative;
-  z-index: -1;
+  grid-column: 2 / span 8;
+  grid-row: 3 / span 13;
+  z-index: -1
+
 `
 
 export default function Blog({ allPosts }) {
 
   const posts = allPosts.map(post =>
     <Post key={post.id}>
-      <BlogID>{`#${post.id}`}</BlogID>
-      <Wrapper>
+      <Item1>
+        <BlogID>{`#${post.id}`}</BlogID>
         <PostImg src={post.img_URL} alt={post.img_URL} />
-        <PostLink to={`/blog/${post.id}`}>
-          <ContentWrapper>
-            <PostTitle>{post.main_title}</PostTitle>
-            <PostSubTitle>{post.subtitle}</PostSubTitle>
-          </ContentWrapper>
-        </PostLink>
-      </Wrapper>
+      </Item1>
       <ColorSquare>
       </ColorSquare>
-    </Post>
+      <TitleWrapper>
+        <PostLink to={`/blog/${post.id}`}>
+          <PostTitle>{post.main_title}</PostTitle>
+          <PostSubTitle>{post.subtitle}</PostSubTitle>
+        </PostLink>
+      </TitleWrapper>
+      <ColorSquare>
+      </ColorSquare>
+    </Post >
   )
-  
+
   return (
-    <Main>
+    // <Main>
       <PostsWrapper>
         {posts}
       </PostsWrapper>
-    </Main>
+
+    // </Main>
   )
 }
