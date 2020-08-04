44b10f1104ebd280f4b9c34b8b102cd0ac1d2329
stefonsimmons <s.j.simmons@spartans.nsu.edu>
2020-08-04 12:34:33 -0400

    added draft.js export libraries

--- client/src/components/MyEditor.jsx
+++ client/src/components/MyEditor.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { EditorState } from 'draft-js';
+import { EditorState, convertFromRaw, convertToRaw } from 'draft-js';
 import { Editor } from 'react-draft-wysiwyg';
+import {stateToHTML} from 'draft-js-export-html';
 
 
 class MyEditor extends Component {
@@ -19,14 +20,20 @@ class MyEditor extends Component {
 
   render() {
     const { editorState } = this.state;
+    const rawText = convertToRaw(editorState.getCurrentContent())
     return (
       <div>
-        <Editor
-          editorState={editorState}
-          wrapperClassName="rich-editor demo-wrapper"
-          editorClassName="demo-editor"
-          onEditorStateChange={this.onEditorStateChange}
-        />
+        <div>
+          {/* {console.log(stateToHTML(editorState.getCurrentContent()))} */}
+          {/* {console.log(convertToRaw(editorState.getCurrentContent()))} */}
+          {/* {console.log(stateToHTML(convertFromRaw(rawText)))} */}
+          <Editor
+            editorState={editorState}
+            wrapperClassName="rich-editor demo-wrapper"
+            editorClassName="demo-editor"
+            onEditorStateChange={this.onEditorStateChange}
+          />
+        </div>
       </div>
     );
   }
