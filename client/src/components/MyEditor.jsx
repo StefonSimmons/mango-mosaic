a70f5527474dbeff499cdaa8ab73b5f98f848114
stefonsimmons <s.j.simmons@spartans.nsu.edu>
2020-08-05 00:40:34 -0400

    showing draft text editor and able to toggle toolbar

--- client/src/components/MyEditor.jsx
+++ client/src/components/MyEditor.jsx
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
+import { PreviewModal } from './PreviewModal';
+
 import { EditorState, convertFromRaw, convertToRaw } from 'draft-js';
 import { Editor } from 'react-draft-wysiwyg';
-import {stateToHTML} from 'draft-js-export-html';
+// import { stateToHTML } from 'draft-js-export-html';
+import draftToHtml from 'draftjs-to-html'
+// import parse from 'html-react-parser';
+
 
 
 class MyEditor extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      editorState: EditorState.createEmpty()
+      editorState: EditorState.createEmpty(),
+      toolBar: true
     };
   }
 
@@ -17,25 +23,68 @@ class MyEditor extends Component {
     this.setState({ editorState });
   };
 
+  hideToolBar = () => {
+    this.setState(prevState => ({
+      toolBar: !prevState.toolBar
+    }))
+  }
 
   render() {
-    const { editorState } = this.state;
-    const rawText = convertToRaw(editorState.getCurrentContent())
+    const { editorState, toolBar } = this.state;
+    // const rawText = convertToRaw(editorState.getCurrentContent())
+    // const html = stateToHTML(editorState.getCurrentContent())
+
+    // const getHtml = editorState => draftToHtml(convertToRaw(editorState.getCurrentContent()))
+
     return (
       <div>
         <div>
+          {/* {console.log(editorState.getCurrentContent())} */}
           {/* {console.log(stateToHTML(editorState.getCurrentContent()))} */}
-          {/* {console.log(convertToRaw(editorState.getCurrentContent()))} */}
+          {console.log(convertToRaw(editorState.getCurrentContent()))}
           {/* {console.log(stateToHTML(convertFromRaw(rawText)))} */}
-          <Editor
+          {/* {console.log(parse(stateToHTML(editorState.getCurrentContent())))} */}
+          {/* {console.log(htmlParsed)} */}
+          {/* {console.log(draftToHtml(convertToRaw(editorState.getCurrentContent())))} */}
+          { toolBar ?
+            <Editor
             editorState={editorState}
             wrapperClassName="rich-editor demo-wrapper"
             editorClassName="demo-editor"
             onEditorStateChange={this.onEditorStateChange}
-          />
+          // toolbarClassName='hide-toolbar'
+          // toolbarHidden={true}
+          // readOnly={true}
+            />
+            :
+            <Editor
+              editorState={editorState}
+              wrapperClassName="rich-editor demo-wrapper"
+              editorClassName="demo-editor"
+              onEditorStateChange={this.onEditorStateChange}
+              toolbarClassName='hide-toolbar'
+              toolbarHidden={true}
+              readOnly={true}
+            />
+          }
+          <div>
+            {/* {parse(html)} */}
+          </div>
+          {/* <div className="html-view" dangerouslySetInnerHTML={{ __html: getHtml(editorState) }}>
+            {{ getHtml(editorState) }}
+          </div> */}
+
+          <button onClick={this.hideToolBar}>
+            Preview message
+          </button>
+          {/* <PreviewModal
+            output={getHtml(editorState)}
+          /> */}
         </div>
       </div>
     );
   }
 }
 export { MyEditor };
+
+// className="btn btn-success" data-toggle="modal" data-target="#previewModal"
