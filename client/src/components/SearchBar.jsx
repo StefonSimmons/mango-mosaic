e06a687fe1b65165af1bad265e0c34c4975af42f
stefonsimmons <s.j.simmons@spartans.nsu.edu>
2020-09-05 19:49:53 -0400

    responsive searchbar

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -4,25 +4,36 @@ import styled from 'styled-components'
 import mangoMagnifyIcon from '../assets/mango-magnify-icon.png'
 
 const Search = styled.div`
-  display: flex;
-  justify-content: center;
-  width: 300px;
-  margin-top: 40px
+  width: 85%;
+  margin: 0 auto;
+
+  @media(max-width: 445px){
+    width: 75%;
+    margin-left: 20px 
+  }
 `
 const Bar = styled.input`
   height: 40px;
-  width: 250px;
+  width: 350px;
   border-radius: 15px;
   border: 1px solid grey;
   color: grey;
   padding-left: 50px;
-  margin-bottom: 5px;
+  display: block;
+  margin: 35px auto;
   font-size: 18px;
   background-color: rgba(235,183,45,.075);
   background-image: url(${mangoMagnifyIcon});
   background-repeat: no-repeat;
   background-position: 3% 63%;
   background-size: contain; 
+
+  @media(max-width: 780px){
+    margin-bottom: 5px;
+  }
+  @media(max-width: 480px){
+    width: 250px;
+  }
 
 `
 export default function SearchBar({ getPosts, updateAllPosts }) {
