3f6b7851862551de2aa3d70e274c339d3fe048d5
stefonsimmons <s.j.simmons@spartans.nsu.edu>
2020-09-17 23:49:43 -0400

    space

--- client/src/services/apiConfig.js
+++ client/src/services/apiConfig.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 // api/server runs on localhost 3000 in development
 const baseUrl = process.env.NODE_ENV !== 'production' ? 'https://mango-mosaic-api.herokuapp.com/' : 'http://localhost:3000' 
 
-console.log(process.env)
+console.log(process.env) 
 const api = axios.create({
   baseURL: baseUrl
 })
