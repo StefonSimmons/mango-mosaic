de0910e807d0de28dbe2dc6accfb2b42f1f41e34
stefonsimmons <s.j.simmons@spartans.nsu.edu>
2020-09-23 18:19:21 -0400

    new ruby version and reconfig apiConfig

--- client/src/services/apiConfig.js
+++ client/src/services/apiConfig.js
@@ -1,9 +1,8 @@
 import axios from 'axios'
 
 // api/server runs on localhost 3000 in development
-const baseUrl = process.env.NODE_ENV !== 'production' ? 'https://mango-mosaic-api.herokuapp.com/' : 'http://localhost:3000' 
+const baseUrl = process.env.NODE_ENV === 'production' ? 'https://mango-mosaic-api.herokuapp.com/' : 'http://localhost:3000' 
 
-console.log(process.env) 
 const api = axios.create({
   baseURL: baseUrl
 })
