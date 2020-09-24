b66f121ac8f7f139472a92fe4f72aa33f2fc3105
stefonsimmons <s.j.simmons@spartans.nsu.edu>
2020-09-23 21:01:35 -0400

    testing auth

--- client/src/services/auth.js
+++ client/src/services/auth.js
@@ -2,8 +2,9 @@
 import api from './apiConfig';
 
 export const loginUser = async (loginParams) => {
+  console.log(loginParams)
   const resp = await api.post('/auth/login', { auth: loginParams })
-
+  console.log(resp)
     const token = resp.data.token
     const admin = resp.data.user
     localStorage.setItem('authToken', token);
